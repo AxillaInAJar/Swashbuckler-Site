179b75be4507f2b2024405aa7e86e94239d1e795
Max Mason <maxmason2000@gmail.com>
2015-11-08 15:20:04 -0500

    If this doesn't fix it..

--- js/run.js
+++ js/run.js
@@ -6,10 +6,13 @@ function openLink(url) {
 }
 
 function socialMedia() {
-  facebook = '<img src="images/FB-logo.png" style="width:100%; display:block;"/>';
-  youtube = '<img src="images/YT-logo.png" style="width:100%; display:block;"/>';
-  twitter = '<img src="images/TW-logo.png" style="width:100%; display:block;"/>';
+  facebook = '<img src="images/FB-logo.png" style="width:100%;" id="fb"/>';
+  youtube = '<img src="images/YT-logo.png" style="width:100%;" id="yt" />';
+  twitter = '<img src="images/TW-logo.png" style="width:100%;" id="tw" />';
   document.getElementById('social').innerHTML = facebook + "<br/><br/> " + youtube + "<br/><br/> " + twitter;
+  document.getElementById('fb').style.display = "block"; document.getElementById('fb').style.visibility = "visible";
+  document.getElementById('yt').style.display = "block"; document.getElementById('yt').style.visibility = "visible";
+  document.getElementById('tw').style.display = "block"; document.getElementById('tw').style.visibility = "visible";
 }
 
 window.onload = function() {
