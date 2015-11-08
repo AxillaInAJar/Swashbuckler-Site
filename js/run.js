1814d65826c40817a0484879a70208ccca21115a
Max Mason <maxmason2000@gmail.com>
2015-11-08 14:57:39 -0500

    Fuckload of changes. (Please add links to social media.)

--- js/run.js
+++ js/run.js
@@ -1,8 +1,17 @@
 // Also called: challenge.js
-// In honour of Josh Goldfish 
+// In honour of Josh Goldfish
 
 function openLink(url) {
-
   window.open(url, "_self", "");
+}
 
+function socialMedia() {
+  facebook = '<img src="images/FB-logo.png" style="width:100%;"/>';
+  youtube = '<img src="images/YT-logo.png" style="width:100%;"/>';
+  twitter = '<img src="images/TW-logo.png" style="width:100%;"/>';
+  document.getElementById('social').innerHTML = facebook + "<br/><br/> " + youtube + "<br/><br/> " + twitter;
+}
+
+window.onload = function() {
+  socialMedia();
 }
